f89aefc0107378d0f8a28e8a3660c3c55764da44
Nero5023 <502350123@qq.com>
2015-11-05 19:54:31 +0800

    Complete minium requirement

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,13 +13,29 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM matches")
+    conn.commit()
+    conn.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM players")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT count(*) as num FROM players")
+    results = cursor.fetchall()
+    conn.close()
+    return results[0][0]
 
 
 def registerPlayer(name):
@@ -31,6 +47,12 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(r'''INSERT INTO players(name, win_matches, total_matches) 
+                       VALUES(%s,0,0)''', (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -46,6 +68,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(r'''SELECT * FROM ordered_players''')
+    results = cursor.fetchall()
+    conn.close()
+    return results
 
 
 def reportMatch(winner, loser):
@@ -55,6 +83,24 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('''UPDATE players
+                      SET win_matches = win_matches + 1,
+                          total_matches = total_matches + 1
+                      WHERE id = %d'''%winner)
+    conn.commit()
+    cursor.execute('''UPDATE players
+                      SET total_matches = total_matches + 1
+                      WHERE id = %d'''%loser)
+    conn.commit()
+    next_matches_id = getMaxMatchId() + 1
+    cursor.execute('''INSERT INTO matches(player_id, is_winner, round, match_id)
+                      VALUES(%d, TRUE, 1, %d)'''%(winner, next_matches_id))
+    
+    cursor.execute('''INSERT INTO matches(player_id, is_winner, round, match_id)
+                      VALUES(%d, FALSE, 1, %d)'''%(loser, next_matches_id))
+    conn.commit()
  
  
 def swissPairings():
@@ -72,5 +118,52 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    # max_round = getMaxRound()
+    # current_round = max_round+1
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('''SELECT id, name , win_matches FROM ordered_players''')
+    ordered_players = cursor.fetchall()
+    paris_of_players = []
+    for i in range(len(ordered_players)/2):
+        id1 = ordered_players[2*i][0]
+        name1 = ordered_players[2*i][1]
+        id2 = ordered_players[2*i+1][0]
+        name2 = ordered_players[2*i+1][1]
+        pair = (id1, name1, id2, name2)
+        paris_of_players.append(pair)
+    return paris_of_players
 
+
+def getMaxRound():
+    """Retuen the max round of the tournament.
+
+    Reruens:
+      The max round of the current state.
+    """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT max(round) FROM matches")
+    results = cursor.fetchall()
+    conn.close()
+    return results[0][0]
+
+
+    
+def getMaxMatchId():
+    """Retuen the max match_id of the tournament.
+
+    Reruens:
+      The max match_id of the current state.
+    """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT max(match_id) FROM matches")
+    results = cursor.fetchall()
+    conn.close()
+    max_id = results[0][0]
+    if max_id == None:
+        max_id = 0
+    return max_id
+    
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -7,3 +7,22 @@
 -- these lines here.
 
 
+CREATE TABLE players ( id SERIAL PRIMARY KEY,
+					   name TEXT,
+					   win_matches INTEGER,
+					   total_matches INTEGER );
+
+CREATE TABLE matches ( 
+	player_id SERIAL REFERENCES players(id),
+					   match_id SERIAL,
+					   is_winner BOOLEAN,
+					   round INTEGER );
+
+CREATE VIEW V_matches 
+AS SELECT a.match_id, a.player_id AS winner_id, b.player_id AS loser_id, a.round 
+FROM matches as a, matches as b
+WHERE a.round = b.round and a.match_id = b.match_id 
+	  and a.is_winner = TRUE and b.is_winner = FALSE;
+
+CREATE VIEW ordered_players 
+AS SELECT * FROM players ORDER BY win_matches DESC;

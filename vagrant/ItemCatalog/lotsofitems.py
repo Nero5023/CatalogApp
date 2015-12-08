5b9e8a8f7f59ca6379d6888dfa288a4ea8eaec78
Nero5023 <502350123@qq.com>
2015-12-09 00:00:45 +0800

    Finished database init design , make some fake data, index page.

---
+++ vagrant/ItemCatalog/lotsofitems.py
@@ -0,0 +1,105 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Category, Item
+from datetime import datetime
+from random import randint
+
+engine = create_engine('sqlite:///categoryitems.db')
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+def randomTime():
+	return datetime(2015, randint(3,12), randint(1,30), 
+				randint(1,24), randint(1,60))
+
+
+category1 = Category(name = 'Soccer')
+
+session.add(category1)
+session.commit()
+
+item1 = Item(name='Jersey', description='Description1', 
+			date_time=randomTime(), category=category1)
+session.add(item1)
+session.commit()
+
+item2 = Item(name='Soccer Cleats', description='Description2',
+			date_time=randomTime(), category=category1)
+session.add(item2)
+session.commit()
+
+item3 = Item(name='Two shinguards', description='Description3',
+			date_time=randomTime(), category=category1)
+session.add(item3)
+session.commit()
+
+item4 = Item(name='Shinguards', description='Description4',
+			date_time=randomTime(), category=category1)
+session.add(item4)
+session.commit()
+
+category2 = Category(name='Basketball')
+session.add(category2)
+session.commit()
+
+category3 = Category(name='Baseball')
+session.add(category3)
+session.commit()
+
+item5 = Item(name='Bat', description='Description5',
+			date_time=randomTime(), category=category3)
+session.add(item5)
+session.commit()
+
+category4 = Category(name='Frisbee')
+session.add(category4)
+session.commit()
+
+item6 = Item(name='Frisbee', description='Description6',
+			date_time=randomTime(), category=category4)
+session.add(item6)
+session.commit()
+
+category5 = Category(name='Snowboarding')
+session.add(category5)
+session.commit()
+
+item7 = Item(name='Googles', description='Description7',
+			date_time=randomTime(), category=category5)
+session.add(item7)
+session.commit()
+
+item8 = Item(name='Snowboard', description='Description8',
+			date_time=randomTime(), category=category5)
+session.add(item8)
+session.commit()
+
+category6 = Category(name='Rock Climbing')
+session.add(category6)
+session.commit()
+
+category7 = Category(name='Foosball')
+session.add(category7)
+session.commit()
+
+category8 = Category(name='Skating')
+session.add(category8)
+session.commit()
+
+category9 = Category(name='Hockey')
+session.add(category9)
+session.commit()
+
+item9 = Item(name='Stick', description='Description9',
+			date_time=randomTime(), category=category9)
+session.add(item9)
+session.commit()
+
+print "Added items!"
+
+
---
+++ vagrant/ItemCatalog/project.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc, desc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
+
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///categoryitems.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# https://teach.mozilla.org/activities/
+# Show all categories and latest items
+@app.route('/')
+@app.route('/Category')
+def showCategories():
+	categories = session.query(Category).order_by(asc(Category.name)).all()
+	items = session.query(Item).order_by(desc(Item.date_time)).limit(9)
+	return render_template('categories.html', categories=categories,
+							items=items)
+
+# # Show all the items of one category
+# @app.route('/category/<path:category_name>/')
+# @app.route('/category/<path:category_name>/items/')
+# def showCategoryItems():
+# 	items = session.query(Item).order_by(desc(Item.date_time))
+
+# @app.route('/category/<path:category_name>/<path:item_name>')
+# def showItemDetail():
+# 	category = session.query(Category).filter_by(name=category_name).one()
+# 	items = session.query(Item).filter_by(category_id=category.id, 
+# 											name=item_name).all()
+
+# @app.route('/')
+
+
+
+if __name__ == '__main__':
+	app.secret_key = 'super_secret_key'
+	app.debug = True
+	app.run(host='0.0.0.0', port=8000)
